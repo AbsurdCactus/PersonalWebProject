6831c7111983a8e845e2a01e4ede251b87c61255
Clay Reber <bcreber@bsu.edu>
2020-07-01 12:59:27 -0400

    intro to javascript

---
+++ JavaScript Files/notes_for_javascript.js
@@ -0,0 +1,30 @@
+// 5 Primitive Dataypes. Numbers, Strings, Booleans, Null, and undefined.
+
+//Javascript doesn't care if a number is an integer or a float
+//Modulus is the same.(remainder)
+
+"she said \"goodbye\""  //this will help you add quotations in javascript (ESCAPE CHARACTERS)
+
+//concatenation exists
+"hey guys".length  //strings have a length property
+
+"howdy"[0] //accessing individual characters with an index
+
+var yourName = "jimmy" //this is how you declare a variable
+
+//DYNAMIC TYPING: where a veriable can change from one datatype to another without any problem. 
+
+//undefined just means you have not given a variable any value yet
+var name;
+
+//Null means EXPLICITELY NOTHINGNESS
+var currentPlayer = null;
+
+//Think of methods as a library of pre-written code
+
+alert("duck dad")//just a method which gives off an alert
+
+
+//prompt lets a prompt come up in the browser
+prompt("what is your name")
+
